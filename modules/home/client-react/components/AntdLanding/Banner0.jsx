92c4a9e2c5704c144473c6c2706127e8824b15d7
lokeshrana9999 <lokeshrana9999@gmail.com>
2020-10-10 17:44:04 +0530

    suggested locations done

--- modules/home/client-react/components/AntdLanding/Banner0.jsx
+++ modules/home/client-react/components/AntdLanding/Banner0.jsx
@@ -11,6 +11,8 @@ import {
   Card,
   Skeleton,
   Avatar,
+  Divider,
+  Typography,
 } from "antd";
 import { mapMarkerAlt } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -20,6 +22,7 @@ import TweenOne from "rc-tween-one";
 import { isImg } from "./utils";
 
 const { Option } = Select;
+const { Title } = Typography;
 const { Meta } = Card;
 
 const Banner00DataSource = {
@@ -48,6 +51,8 @@ class Banner extends React.PureComponent {
       searchParam: null,
       loading: false,
       data: null,
+      suggestLoading: true,
+      suggestData: null,
     };
   }
 
@@ -77,6 +82,28 @@ class Banner extends React.PureComponent {
     // }
   };
 
+  componentDidMount() {
+    fetch(urlP + "&limit=5")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            suggestData: result,
+            suggestLoading: false,
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            suggestLoading: false,
+            error,
+          });
+        }
+      );
+  }
+
   getData = () => {
     const { searchParam } = this.state;
     fetch(urlP + searchParam + "&limit=5")
@@ -101,7 +128,14 @@ class Banner extends React.PureComponent {
   };
   render() {
     const { ...currentProps } = this.props;
-    const { data, searchParam, loading, visible } = this.state;
+    const {
+      data,
+      searchParam,
+      loading,
+      visible,
+      suggestLoading,
+      suggestData,
+    } = this.state;
     const dataSource = Banner00DataSource;
     delete currentProps.isMobile;
 
@@ -109,73 +143,45 @@ class Banner extends React.PureComponent {
     console.log("dataP", searchParam, data, loading);
 
     return (
-      <div {...currentProps} {...dataSource.wrapper}>
-        <QueueAnim
-          key="QueueAnim"
-          type={["bottom", "top"]}
-          delay={200}
-          {...dataSource.textWrapper}
-        >
-          {/* <div key="title" {...dataSource.title}>
+      <div>
+        <div {...currentProps} {...dataSource.wrapper}>
+          <QueueAnim
+            key="QueueAnim"
+            type={["bottom", "top"]}
+            delay={200}
+            {...dataSource.textWrapper}
+          >
+            {/* <div key="title" {...dataSource.title}>
             {typeof dataSource.title.children === 'string' && dataSource.title.children.match(isImg) ? (
               <img src={dataSource.title.children} width="100%" alt="img" />
             ) : (
               dataSource.title.children
             )}
           </div> */}
-          <div key="content" className="banner0-title">
-            Home away from Home
-          </div>
-          <br />
-          <br />
-          <br />
-          <Row className="banner-select">
-            <Col span={20}>
-              <Dropdown
-                placement="bottomCenter"
-                visible={visible}
-                overlay={
-                  <Menu style={{ paddingBottom: "0" }}>
-                    {loading ||
-                    !searchParam ||
-                    (searchParam && searchParam.length < 3) ||
-                    !data ||
-                    (data && !data.data) ||
-                    (data &&
-                      data.data &&
-                      data.data.result &&
-                      data.data.result.length == 0) ? (
-                      [...Array(3).keys()].map((kk) => (
-                        <Menu.Item key={kk}>
-                          <Card
-                            bordered={false}
-                            style={{
-                              borderRadius: "0",
-                              borderBottom: "2px solid #f3f3f3",
-                            }}
-                            bodyStyle={{ padding: "10px" }}
-                          >
-                            <Meta
-                              avatar={
-                                <div style={{ padding: "10px" }}>
-                                  <Avatar
-                                    size={25}
-                                    src="https://res.cloudinary.com/approxyma/image/upload/v1602229648/1479569_p9ehdf.svg"
-                                  />
-                                </div>
-                              }
-                              title={
-                                <div style={{marginTop:'-15px'}}>
-                                <Skeleton  active paragraph={{ rows: 1 }} /></div>
-                              }
-                            />
-                          </Card>
-                        </Menu.Item>
-                      ))
-                    ) : (
-                      <>
-                        {data.data.result.map((item, key) => (
-                          <Menu.Item key={key}>
+            <div key="content" className="banner0-title">
+              Home away from Home
+            </div>
+            <br />
+            <br />
+            <br />
+            <Row className="banner-select">
+              <Col span={20}>
+                <Dropdown
+                  placement="bottomCenter"
+                  visible={visible}
+                  overlay={
+                    <Menu style={{ paddingBottom: "0" }}>
+                      {loading ||
+                      !searchParam ||
+                      (searchParam && searchParam.length < 3) ||
+                      !data ||
+                      (data && !data.data) ||
+                      (data &&
+                        data.data &&
+                        data.data.result &&
+                        data.data.result.length == 0) ? (
+                        [...Array(3).keys()].map((kk) => (
+                          <Menu.Item key={kk}>
                             <Card
                               bordered={false}
                               style={{
@@ -193,40 +199,70 @@ class Banner extends React.PureComponent {
                                     />
                                   </div>
                                 }
-                                title={item.name}
-                                description={
-                                  <p style={{ marginTop: "-5px" }}>
-                                    {item.secondary_name != ""
-                                      ? item.secondary_name
-                                      : item.region_type}
-                                  </p>
+                                title={
+                                  <div style={{ marginTop: "-15px" }}>
+                                    <Skeleton active paragraph={{ rows: 1 }} />
+                                  </div>
                                 }
                               />
                             </Card>
                           </Menu.Item>
-                        ))}
-                      </>
-                    )}
-                  </Menu>
-                }
-              >
-                <Input
-                  onFocus={() => {
-                    this.setVisible(true);
-                  }}
-                  onBlur={() => {
-                    this.setVisible(false);
-                  }}
-                  placeholder="input search text"
-                  enterButton="Search"
-                  size="large"
-                  onChange={(e) => {
-                    this.onSearch(e.target.value);
-                  }}
-                  // onSearch={value => console.log(value)}
-                />
-              </Dropdown>
-              {/* <Select
+                        ))
+                      ) : (
+                        <>
+                          {data.data.result.map((item, key) => (
+                            <Menu.Item key={key}>
+                              <Card
+                                bordered={false}
+                                style={{
+                                  borderRadius: "0",
+                                  borderBottom: "2px solid #f3f3f3",
+                                }}
+                                bodyStyle={{ padding: "10px" }}
+                              >
+                                <Meta
+                                  avatar={
+                                    <div style={{ padding: "10px" }}>
+                                      <Avatar
+                                        size={25}
+                                        src="https://res.cloudinary.com/approxyma/image/upload/v1602229648/1479569_p9ehdf.svg"
+                                      />
+                                    </div>
+                                  }
+                                  title={item.name}
+                                  description={
+                                    <p style={{ marginTop: "-5px" }}>
+                                      {item.secondary_name != ""
+                                        ? item.secondary_name
+                                        : item.region_type}
+                                    </p>
+                                  }
+                                />
+                              </Card>
+                            </Menu.Item>
+                          ))}
+                        </>
+                      )}
+                    </Menu>
+                  }
+                >
+                  <Input
+                    onFocus={() => {
+                      this.setVisible(true);
+                    }}
+                    onBlur={() => {
+                      this.setVisible(false);
+                    }}
+                    placeholder="input search text"
+                    enterButton="Search"
+                    size="large"
+                    onChange={(e) => {
+                      this.onSearch(e.target.value);
+                    }}
+                    // onSearch={value => console.log(value)}
+                  />
+                </Dropdown>
+                {/* <Select
               allowClear
               autoClearSearchValue={false}
                 defaultValue={searchParam}
@@ -250,13 +286,13 @@ class Banner extends React.PureComponent {
                 <Option value="lucy">Lucy</Option>
                 <Option value="tom">Tom</Option>
               </Select> */}
-            </Col>
-            <Col span={4}>
-              <Button type="primary" block icon="search" />
-            </Col>
-          </Row>
-        </QueueAnim>
-        {/* <TweenOne
+              </Col>
+              <Col span={4}>
+                <Button type="primary" block icon="search" />
+              </Col>
+            </Row>
+          </QueueAnim>
+          {/* <TweenOne
           animation={{
             y: '-=20',
             yoyo: true,
@@ -268,6 +304,43 @@ class Banner extends React.PureComponent {
         >
           <Icon type="down" />
         </TweenOne> */}
+        </div>
+        <Divider />
+        <div style={{ position: "relative" }}>
+          <br />
+          <div style={{ maxWidth: "1200px", margin: "auto" }}>
+            <Title>Suggested Location</Title>
+
+            <Row gutter={24}>
+              {suggestLoading ||
+                !suggestData ||
+                (suggestData && !suggestData.data) ||
+                (suggestData &&
+                suggestData.data &&
+                suggestData.data.result &&
+                suggestData.data.result.length == 0
+                  ? [...Array(3).keys()].map((kk) => (
+                      <Col key={kk} xs={24} md={8} lg={6}>
+                        <Card style={{ marginBottom: "10px" }}>
+                          <Skeleton active paragraph={{ rows: 1 }} />
+                        </Card>
+                      </Col>
+                    ))
+                  : suggestData.data.result.map((rest, keu) => (
+                      <Col key={keu} xs={24} md={8} lg={6}>
+                        <Card
+                          style={{ marginBottom: "10px" }}
+                          title={rest.name}
+                        >
+                          {rest.secondary_name != ""
+                            ? rest.secondary_name
+                            : rest.region_type}
+                        </Card>
+                      </Col>
+                    )))}
+            </Row>
+          </div>
+        </div>
       </div>
     );
   }
